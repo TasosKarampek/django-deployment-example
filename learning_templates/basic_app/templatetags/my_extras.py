3a6d47f39f2cdaab0625f1a2a6b8aefde9dd5b80
TasosKarampek <21012hua@gmail.com>
2023-12-14 14:47:46 +0200

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='cut')
+def cut(value,args):
+    return value.replace(args, "")
+
+
+# register.filter('cut', cut)
